50101b8840ab1a2f1c22e39cdef129ca7d606269
Diego Araujo <diego_shakan@hotmail.com>
2019-12-31 11:06:58 -0300

    add annotate gem and create list in models e tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,25 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  first_name             :string
+#  last_name              :string
+#  birthday               :date
+#
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, :last_name, :birthday, presence: true
 end
