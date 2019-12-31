d08dbc87cb9b9a449cf169fb57139ca1cb0b0492
Diego Araujo <diego_shakan@hotmail.com>
2019-12-31 14:17:55 -0300

    add new fields to registration user

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 user = User.new(first_name: "user", last_name:"example", gender_id: 1, schooling_id: 6, 
-                birthday:"01-01-1999", email: "user@example.com", password: "changeme", 
-                password_confirmation: "changeme")
+                birthday:"01-01-1999", phone: "84991112233", email: "user@example.com", 
+                password: "changeme", password_confirmation: "changeme")
 user.save
 
 Dir[File.join(Rails.root, 'db', 'seeds', '*.rb')].sort.each { |seed| load seed }
